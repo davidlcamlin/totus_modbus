a20a329e1dfdeb74b8cad558c9df62d4a4eee29c
David Luca <d.luca@camlintechnologies.com>
2014-09-29 11:59:28 +0100

    work in progrss for python example

--- totus_pymod/totus_pymod.py
+++ totus_pymod/totus_pymod.py
@@ -1,9 +1,14 @@
 from pymodbus.client.sync import ModbusTcpClient
+#from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
 
+try:
+    client = ModbusTcpClient('127.0.0.1')
+    result = client.read_input_registers(1,8)
+    print result.bits[0]
+
+    client.close()
+except:
+    print "Exception"
 
 
-client = ModbusTcpClient('127.0.0.1')
-result = client.read_coils(1,1)
-print result.bits[0]
-client.close()
